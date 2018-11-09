7ab81c4afa271057a977a5c0ac9d9f3e559a3a85
Kateryna Bielievantseva <kate.bielievantseva@ohmycode.studio>
2018-11-09 12:00:30 +0200

    edit company page

---
+++ public/design/src/components/CompanyNews/index.js
@@ -0,0 +1,13 @@
+import Base from '../../Base.js';
+
+export default class CompanyNews extends Base {
+	initDOMElements(e) {
+        this.els = {
+        	// _infoTabs: $('.info__tabs'),
+        }
+    }
+
+	onDOMReady(e) {
+		
+	}
+}
