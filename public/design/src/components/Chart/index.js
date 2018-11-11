e2d64224d4341fe948812838305feffd349e4873
Kateryna Bielievantseva <kate.bielievantseva@ohmycode.studio>
2018-11-11 17:53:03 +0200

    chart

--- public/design/src/components/Chart/index.js
+++ public/design/src/components/Chart/index.js
@@ -1,402 +1,147 @@
 import Base from '../../Base.js';
-// import Chart from '../../../node_modules/chart.js/dist/Chart.js';
-// import Chart from '../../../node_modules/chart.js/dist/Chart.bundle.js';
-// import {Chart} from 'chart.js/dist/Chart.bundle.js';
-// import Chart from 'chart.js';
-// import { Chart } from 'chart.js';
-// import * as Chart from './Chart.bundle.min.js';
-// import 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js'
 import Chartist from 'chartist';
+// import Tooltip from 'chartist-plugin-tooltips';
+import tooltip from 'chartist-plugin-tooltips';
 // import * as d3 from 'd3';
 
 export default class Chart extends Base {
 	initDOMElements(e) {
         this.els = {
-        	// _headerNav: $('.header__nav')
+        	_chartFilterListTerm: $('.chart__filter-list--term'),
+        	_chartFilterLinkTerm: $('.chart__filter-list--term .chart__filter-link')
         }
     }
 
 	onDOMReady(e) {
-		// var myChart = new Chart(ctx, {...});
+		var data = {
+			minute: {},
+			hour: {},
+			day: {
+				labels: [
+					'2:00', '4:00', '6:00', '8:00', '10:00', 
+					'12:00', '14:00', '16:00', '18:00', '20:00',
+					'22:00', '24:00', '00:00'	
+				],
+				series: [
+					315, 272, 234, 289, 300,
+					215, 172, 224, 109, 150,
+					215, 232, 314.86
+				]
+			},
+			week: {
+				labels: [
+					'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 
+					'Sat', 'Sun'
+				],
+				series: [
+					112, 72, 234, 289, 300,
+					215, 172
+				]
+			},
+			month: {},
+			quarter: {},
+			year: {
+				labels: [
+					'Jan', 'Feb', 'Mar', 'Apr', 'May', 
+					'Jun', 'Jul', 'Aug', 'Sep', 'Oct',
+					'Nov', 'Dec'
+				],
+				series: [
+					112, 72, 234, 289, 300,
+					215, 172, 224, 109, 150,
+					215, 232, 
+				]
+			},
+			allTime: {}
+		}
 
-		/*var myLineChart = new Chart(ctx, {
-			type: 'line',
-			data: data,
-			options: options
-		});*/
-
-		// var ctx = document.getElementById("myChart");
-		// var ctx = document.getElementById("myChart").getContext("2d");
-		// var ctx = $("#myChart");
-		// var ctx = "myChart";
-	
-
-
-		/*var ctx = document.getElementById("myChart").getContext('2d');
-		// var ctx = document.getElementById("myChart");
-		var myChart = new Chart(ctx, {
-		    type: 'bar',
-		    data: {
-		        labels: ["Red", "Blue", "Yellow"],
-		        datasets: [{
-		            label: '# of Votes',
-		            data: [12, 19, 3],
-		            backgroundColor: [
-		                'rgba(255, 99, 132, 0.2)',
-		                'rgba(54, 162, 235, 0.2)',
-		                'rgba(255, 206, 86, 0.2)',
-		            ],
-		            borderColor: [
-		                'rgba(255, 99, 132,1)',
-		                'rgba(54, 162, 235, 1)',
-		                'rgba(255, 206, 86, 1)'
-		            ],
-		            borderWidth: 1
-		        }]
-		    }
-		});*/
-
-
-
-
-
-
-
-
-
-		// import 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js'
-      
-	      /*const canvas = document.getElementById("chart");
-	      let chart = new Chart(canvas, {
-	        type: "line",
-	        data: {
-	          labels: ["1", "2", "3", "4", "5"],
-	          datasets: [{
-	            label: "Series 1",
-	            data: [1, 2, 3, 2, 1]
-	          }]
-	        }
-	      });
-	      console.log(chart)*/
-
-
-
-
-
-
-
-
-
-		/*var ctx = document.getElementById("myChart").getContext('2d');
-		var myChart = new Chart(ctx, {
-			type: 'bar',
-    		data: {
-    			label: ["label"],
-    			backgroundColor: ['red', 'green'],
-    			borderColor: '#000',
-    			data: [5, 20]
-    		},
-    		options: {}
-		});*/
-
-
-
-
-		/*var ctx = document.getElementById("myChart").getContext('2d');
-    var myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero:true
-                    }
-                }]
-            }
-        }
-    });*/
-
-
-
-    /*const square = d3.selectAll("rect");
-	square.style("fill", "orange");*/
-
-
-
-
-
-
-
-
-
-
-
-	var data = {
-	  // A labels array that can contain any sort of values
-	  labels: ['2:00', '4:00', '6:00', '8:00', '10:00', 
-	  		   '12:00', '14:00', '16:00', '18:00', '20:00',
-	  		   '22:00', '24:00', '00:00'],
-	  // Our series array that contains series objects or in this case series data arrays
-	  series: [
-	    [315, 272, 234, 289, 300,
-	     215, 172, 224, 109, 150,
-	     215, 232, 314.86]
-	  ]
-	};
-
-	var options = {
-	  // width: 300,
-	  height: 120,
-	  axisY: {
-	  	offset: 0,
-	    // We can disable the grid for this axis
-	    showGrid: false,
-	    // and also don't show the label
-	    showLabel: false
-	  },
-	  axisX: {
-
-	  },
-	  chartPadding: {
-	    right: 50,
-	    left: 30
-	  },
-	  fullWidth: true,
-	  // lineSmooth: Chartist.Interpolation.step();
-	};
-
-	// Create a new line chart object where as first parameter we pass in a selector
-	// that is resolving to our chart container element. The Second parameter
-	// is the actual data object.
-	new Chartist.Line('.ct-chart', data, options);
-
-
-
-
-
-
-
-
-
-
-
-
-	/*var options = {
-	animationEnabled: true,
-	theme: "light2",
-	title:{
-		text: "Actual vs Projected Sales"
-	},
-	axisX:{
-		valueFormatString: "DD MMM"
-	},
-	axisY: {
-		title: "Number of Sales",
-		suffix: "K",
-		minimum: 30
-	},
-	toolTip:{
-		shared:true
-	},  
-	legend:{
-		cursor:"pointer",
-		verticalAlign: "bottom",
-		horizontalAlign: "left",
-		dockInsidePlotArea: true,
-		itemclick: toogleDataSeries
-	},
-	data: [{
-		type: "line",
-		showInLegend: true,
-		name: "Projected Sales",
-		markerType: "square",
-		xValueFormatString: "DD MMM, YYYY",
-		color: "#F08080",
-		yValueFormatString: "#,##0K",
-		dataPoints: [
-			{ x: new Date(2017, 10, 1), y: 63 },
-			{ x: new Date(2017, 10, 2), y: 69 },
-			{ x: new Date(2017, 10, 3), y: 65 },
-			{ x: new Date(2017, 10, 4), y: 70 },
-			{ x: new Date(2017, 10, 5), y: 71 },
-			{ x: new Date(2017, 10, 6), y: 65 },
-			{ x: new Date(2017, 10, 7), y: 73 },
-			{ x: new Date(2017, 10, 8), y: 96 },
-			{ x: new Date(2017, 10, 9), y: 84 },
-			{ x: new Date(2017, 10, 10), y: 85 },
-			{ x: new Date(2017, 10, 11), y: 86 },
-			{ x: new Date(2017, 10, 12), y: 94 },
-			{ x: new Date(2017, 10, 13), y: 97 },
-			{ x: new Date(2017, 10, 14), y: 86 },
-			{ x: new Date(2017, 10, 15), y: 89 }
-		]
-	},
-	{
-		type: "line",
-		showInLegend: true,
-		name: "Actual Sales",
-		lineDashType: "dash",
-		yValueFormatString: "#,##0K",
-		dataPoints: [
-			{ x: new Date(2017, 10, 1), y: 60 },
-			{ x: new Date(2017, 10, 2), y: 57 },
-			{ x: new Date(2017, 10, 3), y: 51 },
-			{ x: new Date(2017, 10, 4), y: 56 },
-			{ x: new Date(2017, 10, 5), y: 54 },
-			{ x: new Date(2017, 10, 6), y: 55 },
-			{ x: new Date(2017, 10, 7), y: 54 },
-			{ x: new Date(2017, 10, 8), y: 69 },
-			{ x: new Date(2017, 10, 9), y: 65 },
-			{ x: new Date(2017, 10, 10), y: 66 },
-			{ x: new Date(2017, 10, 11), y: 63 },
-			{ x: new Date(2017, 10, 12), y: 67 },
-			{ x: new Date(2017, 10, 13), y: 66 },
-			{ x: new Date(2017, 10, 14), y: 56 },
-			{ x: new Date(2017, 10, 15), y: 64 }
-		]
-	}]
-};
-$("#chartContainer").CanvasJSChart(options);
-
-function toogleDataSeries(e){
-	if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-		e.dataSeries.visible = false;
-	} else{
-		e.dataSeries.visible = true;
+		this.createChart(data.day);
+		// this.els._chartFilterLinkTerm.click((e) => this.filterChart(e, data));
+		this.els._chartFilterListTerm.on('click', 'a:not(.chart__filter-link--active)', 
+			(e) => this.filterChart(e, data));
 	}
-	e.chart.render();
-}
-*/
 
+	createChart(data) {
+		var chart = new Chartist.Line('.ct-chart', {
+			labels: data.labels,
+			series: [
+				data.series
+			]
+		}, 
+		{
+			height: 150,
+			low: 0,
+			lineSmooth: false,
+			fullWidth: true,
+			axisY: {
+				offset: 0,
+			    // showGrid: false,
+			    showLabel: false
+			  },
+			plugins: [
+				Chartist.plugins.tooltip({
+					currency: '$',
+					// anchorToPoint: true,
+					pointClass: 'ct-circle',
+					tooltipOffset: {
+						x: 0,
+						y: 30
+					},
+				})
+			]
+		});
 
+		// replace point to circle for making space between points & lines
+		chart.on('draw', function(data) {
+			if(data.type === 'point') {
+				var circle = new Chartist.Svg('circle', {
+					cx: [data.x],
+					cy: [data.y],
+					r: [4],
+					'ct:value': data.value.y,
+					'ct:meta': data.meta,
+					class: 'ct-circle',
+				});
+		    	data.element.replace(circle);
+			}
+		    $($('.ct-vertical')).css({ 'opacity': 0 });
+		    $($('.ct-vertical')[0]).css({ 'opacity': 1 });
+		});
+	}
 
+	filterChart(e, data) {
+		this.els._chartFilterLinkTerm.removeClass('chart__filter-link--active');
+		$(e.currentTarget).addClass('chart__filter-link--active');
 
-
-
-
-
-/*
-
-	var margin = {top: 50, right: 50, bottom: 50, left: 50}
-  , width = window.innerWidth - margin.left - margin.right // Use the window's width 
-  , height = window.innerHeight - margin.top - margin.bottom; // Use the window's height
-
-// The number of datapoints
-var n = 21;
-
-// 5. X scale will use the index of our data
-var xScale = d3.scaleLinear()
-    .domain([0, n-1]) // input
-    .range([0, width]); // output
-
-// 6. Y scale will use the randomly generate number 
-var yScale = d3.scaleLinear()
-    .domain([0, 1]) // input 
-    .range([height, 0]); // output 
-
-// 7. d3's line generator
-var line = d3.line()
-    .x(function(d, i) { return xScale(i); }) // set the x values for the line generator
-    .y(function(d) { return yScale(d.y); }) // set the y values for the line generator 
-    .curve(d3.curveMonotoneX) // apply smoothing to the line
-
-// 8. An array of objects of length N. Each object has key -> value pair, the key being "y" and the value is a random number
-var dataset = d3.range(n).map(function(d) { return {"y": d3.randomUniform(1)() } })
-
-// 1. Add the SVG to the page and employ #2
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-// 3. Call the x axis in a group tag
-svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(xScale)); // Create an axis component with d3.axisBottom
-
-// 4. Call the y axis in a group tag
-svg.append("g")
-    .attr("class", "y axis")
-    .call(d3.axisLeft(yScale)); // Create an axis component with d3.axisLeft
-
-// 9. Append the path, bind the data, and call the line generator 
-svg.append("path")
-    .datum(dataset) // 10. Binds data to the line 
-    .attr("class", "line") // Assign a class for styling 
-    .attr("d", line); // 11. Calls the line generator 
-
-// 12. Appends a circle for each datapoint 
-svg.selectAll(".dot")
-    .data(dataset)
-  .enter().append("circle") // Uses the enter().append() method
-    .attr("class", "dot") // Assign a class for styling
-    .attr("cx", function(d, i) { return xScale(i) })
-    .attr("cy", function(d) { return yScale(d.y) })
-    .attr("r", 5)
-      .on("mouseover", function(a, b, c) { 
-  			console.log(a) 
-        this.attr('class', 'focus')
-		})
-      .on("mouseout", function() {  })
-//       .on("mousemove", mousemove);
-
-//   var focus = svg.append("g")
-//       .attr("class", "focus")
-//       .style("display", "none");
-
-//   focus.append("circle")
-//       .attr("r", 4.5);
-
-//   focus.append("text")
-//       .attr("x", 9)
-//       .attr("dy", ".35em");
-
-//   svg.append("rect")
-//       .attr("class", "overlay")
-//       .attr("width", width)
-//       .attr("height", height)
-//       .on("mouseover", function() { focus.style("display", null); })
-//       .on("mouseout", function() { focus.style("display", "none"); })
-//       .on("mousemove", mousemove);
-  
-//   function mousemove() {
-//     var x0 = x.invert(d3.mouse(this)[0]),
-//         i = bisectDate(data, x0, 1),
-//         d0 = data[i - 1],
-//         d1 = data[i],
-//         d = x0 - d0.date > d1.date - x0 ? d1 : d0;
-//     focus.attr("transform", "translate(" + x(d.date) + "," + y(d.close) + ")");
-//     focus.select("text").text(d);
-//   }
-*/
-
-
-
+		switch (e.currentTarget.text) {
+			case 'Минута':
+				// this.createChart(data.minute)
+				this.createChart(data.day)
+				break;
+			case 'Час':
+				this.createChart(data.year)
+				// this.createChart(data.hour)
+				break;
+			case 'Неделя':
+				this.createChart(data.week)
+				break;
+			case 'Месяц':
+				// this.createChart(data.month)
+				this.createChart(data.day)
+				break;
+			case 'Квартал':
+				// this.createChart(data.quarter)
+				this.createChart(data.day)
+				break;
+			case 'Год':
+				this.createChart(data.year)
+				break;
+			case 'Все время':
+				// this.createChart(data.allTime)
+				this.createChart(data.month)
+				break;
+			default:
+				this.createChart(data.day)
+		}
 	}
 }
