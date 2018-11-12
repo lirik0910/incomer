585d0606da0658b457e6cdbc04de5a9d741f201c
Ivan Bastryhin <ivan.bastryhin@ohmycode.studio>
2018-11-12 15:54:52 +0200

    blade fixes

--- resources/views/content/index.blade.php
+++ resources/views/content/index.blade.php
@@ -197,7 +197,7 @@
               	<a class="preview__link" href="#">
               	  <img class="preview__img" src="img/placeholder.png" alt="video-preview">
                   <p class="preview__text">Всё о презентации WWDC2018</p>
-                  <button class="preview__btn"><?xml version="1.0" encoding="iso-8859-1"?>
+                  <button class="preview__btn">
 					<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 						 viewBox="0 0 415.346 415.346" style="enable-background:new 0 0 415.346 415.346;" xml:space="preserve">
 						<path d="M41.712,415.346c-11.763,0-21.3-9.537-21.3-21.3V21.299C20.412,9.536,29.949,0,41.712,0l346.122,191.697
@@ -210,7 +210,7 @@
               	<a class="preview__link" href="#">
               	  <img class="preview__img" src="img/placeholder.png" alt="video-preview">
                   <p class="preview__text">Всё о презентации WWDC2018</p>
-                  <button class="preview__btn"><?xml version="1.0" encoding="iso-8859-1"?>
+                  <button class="preview__btn">
 					<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 						 viewBox="0 0 415.346 415.346" style="enable-background:new 0 0 415.346 415.346;" xml:space="preserve">
 						<path d="M41.712,415.346c-11.763,0-21.3-9.537-21.3-21.3V21.299C20.412,9.536,29.949,0,41.712,0l346.122,191.697
