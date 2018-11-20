d2f845e3cd8a9ae4d09334bd3a69cca32ea56f44
Kateryna Bielievantseva <kate.bielievantseva@ohmycode.studio>
2018-11-20 17:13:07 +0200

    graphics on company list page

--- public/design/src/components/CompaniesList/index.js
+++ public/design/src/components/CompaniesList/index.js
@@ -1,5 +1,8 @@
 import Base from '../../Base.js';
 import Isotope from 'isotope-layout';
+import Highcharts from 'highcharts';
+import Exporting from 'highcharts/modules/exporting';
+Exporting(Highcharts);
 
 export default class CompaniesList extends Base {
 	initDOMElements(e) {
@@ -13,6 +16,85 @@ export default class CompaniesList extends Base {
 		this.els._companiesSortLink.click((e) => this.sortingIsotop(e, iso));
 		this.els._companiesSortLink.click((e) => this.dataAttrToggling(e));
 		this.els._companiesSortLink.click((e) => this.toggleActiveClass(e));
+
+
+
+
+		Highcharts.Renderer.prototype.symbols.line = function(x, y, width, height) {
+		    return ['M', x, y, 'L', x + height, y];
+		};
+
+		Highcharts.setOptions({
+			chart: {
+		        height: 60,
+		        width: 327
+		    },
+		    title: false,
+			legend: false,
+		    xAxis: {
+		        visible: false
+		    },
+
+		    yAxis: {
+		        visible: false
+		    },
+		});
+
+		for(var i = 0; i < $("[id^='chart-']").length; i++) {
+			var chart = new Highcharts.Chart({
+		    chart: { renderTo: $("[id^='chart-']")[i] },
+			    series: [{
+			        data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
+			        pointStart: Date.UTC(2010, 0, 1),
+			        pointInterval: 3600 * 1000, // one hour
+			        marker: false,
+			        color: '#252525',
+			        states: {
+		                hover: false
+		            }
+			    }]
+			});
+		}
+
+
+
+
+
+		/*
+		window.chart = new Highcharts.Chart({
+		// Highcharts.chart('chart-1', {
+			chart: {
+				// renderTo: $container[0],
+				renderTo: 'chart-1',
+		        height: 60,
+		        width: 327
+		    },
+		    title: false,
+			legend: false,
+		    xAxis: {
+		        // type: 'datetime'
+		        visible: false
+		    },
+
+		    yAxis: {
+		        visible: false
+		    },
+
+		    series: [{
+		        data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
+		        pointStart: Date.UTC(2010, 0, 1),
+		        pointInterval: 3600 * 1000, // one hour
+		        marker: false,
+		        color: '#252525',
+		        states: {
+	                hover: false
+	            }
+		    }]
+		});*/
+
+
+
+
 	}
 
 	initIsotope() {
