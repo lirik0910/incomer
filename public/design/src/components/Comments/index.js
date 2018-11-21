1648a7e44d3ed3859e73e8817583f6458e7b3b9f
Kateryna Bielievantseva <kate.bielievantseva@ohmycode.studio>
2018-11-21 14:48:06 +0200

    post comments

---
+++ public/design/src/components/Comments/index.js
@@ -0,0 +1,13 @@
+import Base from '../../Base.js';
+
+export default class Comments extends Base {
+	initDOMElements(e) {
+        this.els = {
+        	// _headerNav: $('.header__nav'),
+        }
+    }
+
+	onDOMReady(e) {
+
+	}
+}
