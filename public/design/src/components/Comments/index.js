a5456d38a92e96f073e88ead58a95b409b67e55b
Kateryna Bielievantseva <kate.bielievantseva@ohmycode.studio>
2018-11-21 17:56:25 +0200

    search

--- public/design/src/components/Comments/index.js
+++ public/design/src/components/Comments/index.js
@@ -3,11 +3,17 @@ import Base from '../../Base.js';
 export default class Comments extends Base {
 	initDOMElements(e) {
         this.els = {
+        	_commentsVoteUp: $('.comments__vote-up')
         	// _headerNav: $('.header__nav'),
         }
     }
 
 	onDOMReady(e) {
+		$('.comments__vote-up').click((e) => this.toggleActiveClass(e));
+	}
 
+	toggleActiveClass(e) {
+		e.preventDefault();
+		$(e.currentTarget).toggleClass('active');
 	}
 }
