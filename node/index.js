e3d2c0f3eb6affd5b6d9f4a771f5320ab66d9a71
vlad <vladislavvsmolyaninov@gmail.com>
2018-11-29 12:52:01 +0200

    manager fixes for prod

--- node/index.js
+++ node/index.js
@@ -3,8 +3,9 @@ const rss = require('./rss');
 const chart = require('./chart');
 const cron = require('node-cron');
 //
-// cron.schedule('*/10 * * * *', rss);
-// cron.schedule('*/15 * * * *',() => chart());
-// cron.schedule('0 0 * * *',() => chart('year'));
+cron.schedule('*/10 * * * *', rss);
+cron.schedule('*/15 * * * *',() => chart());
+cron.schedule('0 0 * * *',() => chart('year'));
 // rss();
-chart();
+// chart();
+// chart('year');
