a62579f3021093cce74c70fe79859b7be73cabb1
vlad <vladislavvsmolyaninov@gmail.com>
2018-11-29 13:00:18 +0200

    manager fixes for prod

---
+++ public/manager/precache-manifest.cbac1ee2c07051e1abe3f1d82b436068.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa7d12f6f378b11363a7",
+    "url": "http://95.179.128.38/manager/static/js/11.aa7d12f6.chunk.js"
+  },
+  {
+    "revision": "f5c3f2ff0e629b3021ef",
+    "url": "http://95.179.128.38/manager/static/js/0.f5c3f2ff.chunk.js"
+  },
+  {
+    "revision": "a8571f8256dc0d765863",
+    "url": "http://95.179.128.38/manager/static/js/1.a8571f82.chunk.js"
+  },
+  {
+    "revision": "a4e403fe6a15a879960c",
+    "url": "http://95.179.128.38/manager/static/js/2.a4e403fe.chunk.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "http://95.179.128.38/manager/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "8e58550cec68c786929f",
+    "url": "http://95.179.128.38/manager/static/js/3.8e58550c.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "http://95.179.128.38/manager/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "4e07c1c59affcd9fe47d",
+    "url": "http://95.179.128.38/manager/static/js/4.4e07c1c5.chunk.js"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "http://95.179.128.38/manager/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "7ea57292259ab1998ad4",
+    "url": "http://95.179.128.38/manager/static/js/5.7ea57292.chunk.js"
+  },
+  {
+    "revision": "cd57910881763a749113",
+    "url": "http://95.179.128.38/manager/static/js/6.cd579108.chunk.js"
+  },
+  {
+    "revision": "599ba3a2c8b417586a8a",
+    "url": "http://95.179.128.38/manager/static/js/7.599ba3a2.chunk.js"
+  },
+  {
+    "revision": "d487c3e70805223f87e3",
+    "url": "http://95.179.128.38/manager/static/js/main.d487c3e7.chunk.js"
+  },
+  {
+    "revision": "3bbe29688bedd606491d",
+    "url": "http://95.179.128.38/manager/static/js/9.3bbe2968.chunk.js"
+  },
+  {
+    "revision": "94e8b88a6440f752632a",
+    "url": "http://95.179.128.38/manager/static/js/10.94e8b88a.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "http://95.179.128.38/manager/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "d186e56a9f6968b9bf33",
+    "url": "http://95.179.128.38/manager/static/js/12.d186e56a.chunk.js"
+  },
+  {
+    "revision": "fba42a8dc1c91d173295",
+    "url": "http://95.179.128.38/manager/static/js/13.fba42a8d.chunk.js"
+  },
+  {
+    "revision": "09c7867db2e394d033a5",
+    "url": "http://95.179.128.38/manager/static/js/14.09c7867d.chunk.js"
+  },
+  {
+    "revision": "8a004390621fb88f8c97",
+    "url": "http://95.179.128.38/manager/static/js/15.8a004390.chunk.js"
+  },
+  {
+    "revision": "de0aaf60fce7a55afcc9",
+    "url": "http://95.179.128.38/manager/static/js/16.de0aaf60.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "http://95.179.128.38/manager/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "f2cce3b598450c55dd03",
+    "url": "http://95.179.128.38/manager/static/js/17.f2cce3b5.chunk.js"
+  },
+  {
+    "revision": "479185bbae2861467588",
+    "url": "http://95.179.128.38/manager/static/js/runtime~main.479185bb.js"
+  },
+  {
+    "revision": "e46d34673a4d1335d99396af0cc9ff43",
+    "url": "http://95.179.128.38/manager/static/media/spinner.e46d3467.gif"
+  },
+  {
+    "revision": "7ea57292259ab1998ad4",
+    "url": "http://95.179.128.38/manager/static/css/5.2dbf0972.chunk.css"
+  },
+  {
+    "revision": "4e07c1c59affcd9fe47d",
+    "url": "http://95.179.128.38/manager/static/css/4.2dbf0972.chunk.css"
+  },
+  {
+    "revision": "8e58550cec68c786929f",
+    "url": "http://95.179.128.38/manager/static/css/3.243e1b50.chunk.css"
+  },
+  {
+    "revision": "f2cce3b598450c55dd03",
+    "url": "http://95.179.128.38/manager/static/css/17.b5a5c7a2.chunk.css"
+  },
+  {
+    "revision": "a8571f8256dc0d765863",
+    "url": "http://95.179.128.38/manager/static/css/1.cdaf27bd.chunk.css"
+  },
+  {
+    "revision": "c1169a2f855aff7a6454e0ff00792e97",
+    "url": "http://95.179.128.38/manager/index.html"
+  }
+];
