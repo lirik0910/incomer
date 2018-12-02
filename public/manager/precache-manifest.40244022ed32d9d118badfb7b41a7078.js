4ffdcd6042189c13002051b6b6fb9f7146edebf9
vlad <vladislavvsmolyaninov@gmail.com>
2018-12-02 17:07:14 +0200

    manager build

---
+++ public/manager/precache-manifest.40244022ed32d9d118badfb7b41a7078.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "93a49e63b0f3c39b847d",
+    "url": "http://localhost/manager/static/js/12.93a49e63.chunk.js"
+  },
+  {
+    "revision": "e9cbf697b98979bc60cb",
+    "url": "http://localhost/manager/static/js/0.e9cbf697.chunk.js"
+  },
+  {
+    "revision": "438b99c7e8cdccc4f73d",
+    "url": "http://localhost/manager/static/js/1.438b99c7.chunk.js"
+  },
+  {
+    "revision": "a4e403fe6a15a879960c",
+    "url": "http://localhost/manager/static/js/2.a4e403fe.chunk.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "http://localhost/manager/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "89631a955e73c521d89b",
+    "url": "http://localhost/manager/static/js/3.89631a95.chunk.js"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "http://localhost/manager/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "c0297c8f89bcf63a89af",
+    "url": "http://localhost/manager/static/js/4.c0297c8f.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "http://localhost/manager/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "c11b3cc647498104daaf",
+    "url": "http://localhost/manager/static/js/5.c11b3cc6.chunk.js"
+  },
+  {
+    "revision": "cd57910881763a749113",
+    "url": "http://localhost/manager/static/js/6.cd579108.chunk.js"
+  },
+  {
+    "revision": "947913a5efe24983658c",
+    "url": "http://localhost/manager/static/js/7.947913a5.chunk.js"
+  },
+  {
+    "revision": "2836bda1976ac7d8f7c6",
+    "url": "http://localhost/manager/static/js/main.2836bda1.chunk.js"
+  },
+  {
+    "revision": "424547dabd8ddb26eb44",
+    "url": "http://localhost/manager/static/js/9.424547da.chunk.js"
+  },
+  {
+    "revision": "94e8b88a6440f752632a",
+    "url": "http://localhost/manager/static/js/10.94e8b88a.chunk.js"
+  },
+  {
+    "revision": "80355cb117db6d61e480",
+    "url": "http://localhost/manager/static/js/11.80355cb1.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "http://localhost/manager/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b97d52ee193912aeede7",
+    "url": "http://localhost/manager/static/js/13.b97d52ee.chunk.js"
+  },
+  {
+    "revision": "c58275e810ad11bd8e8e",
+    "url": "http://localhost/manager/static/js/14.c58275e8.chunk.js"
+  },
+  {
+    "revision": "3d8c64f8c4168625c98d",
+    "url": "http://localhost/manager/static/js/15.3d8c64f8.chunk.js"
+  },
+  {
+    "revision": "240910e8cd08c8bd4274",
+    "url": "http://localhost/manager/static/js/16.240910e8.chunk.js"
+  },
+  {
+    "revision": "9e4f485e6d0011619404",
+    "url": "http://localhost/manager/static/js/17.9e4f485e.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "http://localhost/manager/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "452f08fa5dc67d10a8d5",
+    "url": "http://localhost/manager/static/js/18.452f08fa.chunk.js"
+  },
+  {
+    "revision": "c0651719cb6cc054af32",
+    "url": "http://localhost/manager/static/js/runtime~main.c0651719.js"
+  },
+  {
+    "revision": "e46d34673a4d1335d99396af0cc9ff43",
+    "url": "http://localhost/manager/static/media/spinner.e46d3467.gif"
+  },
+  {
+    "revision": "c11b3cc647498104daaf",
+    "url": "http://localhost/manager/static/css/5.2dbf0972.chunk.css"
+  },
+  {
+    "revision": "c0297c8f89bcf63a89af",
+    "url": "http://localhost/manager/static/css/4.2dbf0972.chunk.css"
+  },
+  {
+    "revision": "89631a955e73c521d89b",
+    "url": "http://localhost/manager/static/css/3.243e1b50.chunk.css"
+  },
+  {
+    "revision": "452f08fa5dc67d10a8d5",
+    "url": "http://localhost/manager/static/css/18.01ba164d.chunk.css"
+  },
+  {
+    "revision": "438b99c7e8cdccc4f73d",
+    "url": "http://localhost/manager/static/css/1.cdaf27bd.chunk.css"
+  },
+  {
+    "revision": "10d5bddb0a3f17add8661a75c170fdbd",
+    "url": "http://localhost/manager/index.html"
+  }
+];
