68af483941185f4e3a1bda5783f956e76459ea44
Ivan Bastryhin <ivan.bastryhin@ohmycode.studio>
2018-12-03 12:30:52 +0200

    companies page

--- app/Repositories/Person/PersonEloquent.php
+++ app/Repositories/Person/PersonEloquent.php
@@ -66,7 +66,6 @@ class PersonEloquent implements PersonRepository
             $items->orderBy('name', 'ASC');
         }
 
-
         return $items->get();
         //return $items->offset(($page - 1) * $limit)->simplePaginate($limit);
     }
