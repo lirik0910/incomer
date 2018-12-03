775a13326d62128e7fcabce8d5e8a8e22203ef73
Ivan Bastryhin <ivan.bastryhin@ohmycode.studio>
2018-12-03 12:53:00 +0200

    crypto page

--- resources/views/components/index/hot_news_item.blade.php
+++ resources/views/components/index/hot_news_item.blade.php
@@ -11,7 +11,7 @@
         $img = asset('img/photo-dark.jpg');
     }
 @endphp
-<a class="card @if($type === 'long') card--long-full-img @else card--square-full-img @endif" href="{{ url('/news/' . $item->id) }}" @if($margin) style="margin-right: 0px" @endif>
+<a class="card @if($type === 'long') card--long-full-img @else card--square-full-img @endif" href="{{ url('/news/' . $item->id) }}" @if(isset($margin) && $margin === false ) style="margin-right: 0px" @endif>
     <article class="card__article">
         <div class="card__pic">
             <img src="{{ asset($img) }}">
