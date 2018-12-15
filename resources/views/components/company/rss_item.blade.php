8c4183a8438d30cf25eea5b7d535a91ed66e2f33
Ivan Bastryhin <ivan.bastryhin@ohmycode.studio>
2018-12-15 19:04:47 +0200

    rss view fix

--- resources/views/components/company/rss_item.blade.php
+++ resources/views/components/company/rss_item.blade.php
@@ -2,6 +2,6 @@
     <a class="news__link" target="_blank" href="{{ $item->link }}">
         <span class="news__time">{{ $dateFormatter::format($item->pub_date) }}</span>
         <p class="news__content">{{ $item->title }}</p>
-        <span class="news__site">{{ $item->source }}</span>
+        <!--<span class="news__site">{{ $item->source }}</span>-->
     </a>
 </li>
